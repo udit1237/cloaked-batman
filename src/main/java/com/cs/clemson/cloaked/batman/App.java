d3d8fb50cd339f19d989cb24c9ad5e0bb4d50b04
Emmanuel John <emmylifeline@gmail.com>
2014-04-06 13:04:29 -0400

    init

---
+++ src/main/java/com/cs/clemson/cloaked/batman/App.java
@@ -0,0 +1,13 @@
+package com.cs.clemson.cloaked.batman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
